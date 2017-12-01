3dce29b3f7004e793decd51cd62a6737cb40d36c
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2017-12-01 23:38:11 +0100

    more ideomatic store

--- Modules/Context.js
+++ Modules/Context.js
@@ -1,29 +1,23 @@
 var Observable = require("FuseJS/Observable");
 var Storage = require("FuseJS/Storage");
 
-var profile = Observable();
+var store = {
+    profile: Observable()
+};
 
-function loadProfile() {
-    return Storage.read('profile.json')
-        .then(res => profile.value = JSON.parse(res));
+function load(key) {
+    return Storage.read(key + '.json')
+        .then(res => store[key].value = JSON.parse(res));
 }
 
-function saveProfile() {
-    return Storage.write('profile.json', JSON.stringify(profile.value))
-        .then(res => console.dir(res ? 'Saved successfully!' : 'Save failed!'))
-}
-
-function plateSearch(plateNum) {
-    console.log("running plateSearch: " + plateNum);
-
-    return fetch("http://127.0.0.1:5000/plate/" + plateNum)
-        .then(res => res.json())
-        .then(res => profile.value = res);
+function save(key) {
+    return Storage.write(key + '.json', JSON.stringify(store[key].value))
+        .then(res => console.log(res ? 'Saved successfully!' : 'Save failed!'))
 }
 
 module.exports = {
-    profile: profile,
-    loadProfile: loadProfile,
-    saveProfile: saveProfile,
+    store: store,
+    load: load,
+    save: save,
     plateSearch: plateSearch
 };
--- Pages/ProfilePage.js
+++ Pages/ProfilePage.js
@@ -1,24 +1,32 @@
 var Observable = require("FuseJS/Observable");
 var Context = require("Modules/Context");
 
-var status = Observable("loading");
 var plateNum = Observable("");
 
-Context.loadProfile()
-    .then(() => status.value = "ready",
-    () => status.value = "empty")
-    .then(() => console.dir(status))
+load();
 
-function plateSearch() {
-    Context.plateSearch(plateNum.value);
+function load() {
+    Context.load('profile');
+}
+
+function save() {
+    Context.save('profile');
+}
+
+function plateSearch(plateNum) {
+    console.log("running plateSearch: " + plateNum);
+
+    return fetch("http://127.0.0.1:5000/plate/" + plateNum)
+        .then(res => res.json())
+        .then(res => console.dir(res));
 }
 
 module.exports = {
-    profile: Context.profile,
-    profileJson: Context.profile.map(obj => JSON.stringify(obj)),
-    load: Context.loadProfile,
-    clear: () => Context.profile.value = {},
-    save: Context.saveProfile,
+    profile: Context.store.profile,
+    profileJson: Context.store.profile.map(obj => JSON.stringify(obj)),
+    load: load,
+    clear: () => Context.store.profile.value = {},
+    save: save,
     plateSearch: plateSearch,
     status: status,
     plateNum: plateNum
