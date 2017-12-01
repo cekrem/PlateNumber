d1b14449046162b1070a894c7013eeb33e6fd48c
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2017-12-01 22:52:44 +0100

    implemented simple plate number search

--- Pages/ProfilePage.js
+++ Pages/ProfilePage.js
@@ -1,12 +1,25 @@
 var Observable = require("FuseJS/Observable");
 var Context = require("Modules/Context");
+
 var status = Observable("loading");
+var plateNum = Observable("");
 
 Context.loadProfile()
     .then(() => status.value = "ready",
     () => status.value = "empty")
+    .then(() => console.dir(status))
+
+function plateSearch() {
+    Context.plateSearch(plateNum.value);
+}
 
 module.exports = {
     profile: Context.profile,
-    status: status
+    profileJson: Context.profile.map(obj => JSON.stringify(obj)),
+    load: Context.loadProfile,
+    clear: () => Context.profile.value = {},
+    save: Context.saveProfile,
+    plateSearch: plateSearch,
+    status: status,
+    plateNum: plateNum
 };
